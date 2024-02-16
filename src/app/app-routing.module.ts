1f5bd44dd7b4ab345798b66d5fd664ea70a0a275
Ron Bachar <ron.bachar@outlook.com>
2024-02-16 12:59:27 +0200

    router fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
+import { InfoComponent } from './components/info/info.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { ServicesComponent } from './components/services/services.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TourComponent } from './components/tour/tour.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 
-const routes: Routes =   [
-
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'tour', component: TourComponent },
 ];
 
 @NgModule({
--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-main-nav',
@@ -9,6 +10,9 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrl: './main-nav.component.scss'
 })
 export class MainNavComponent {
+
+
+
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -16,5 +20,8 @@ export class MainNavComponent {
       map(result => result.matches),
       shareReplay()
     );
+
+
+
 
 }
